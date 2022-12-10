bc6faaf317ab0045bbb1bc393f5ae7f16a12a3ad
MarcoDayz <diaz.marco8686@gmail.com>
2022-12-10 11:48:46 -0800

    updated instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -5,8 +5,9 @@ const Instructions = () => {
             <h1>Instructions:</h1>
             <h3>- Create an application that makes an HTTP request to "https://jsonplaceholder.typicode.com/posts".</h3>
             <h3>- Returned data must render all the post titles in a "Titles" component.</h3>
-            <h3>- Application should feature the capability to render a single post once it has been clicked on</h3>
-            <h3>- <span style={{color: 'blue'}}>*Bonues*</span> Create a return button that takes you back to all the posts <span style={{color: 'blue'}}>*Bonues*</span></h3>
+            <h3>- Application should feature the capability to render a single title with the body once it has been clicked on</h3>
+            <h3>- Single body post must be rendered in a "SinglePost" component</h3>
+            <h3><span style={{color: 'blue'}}>*Bonues*</span> Create a return button that takes you back to all the posts <span style={{color: 'blue'}}>*Bonues*</span></h3>
             <h1 style={{fontStyle: 'italic'}}>Create your application below</h1>
         </div>
     )
