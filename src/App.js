875000e5d29a8cb54c55d5015ab21c5d867eb75d
MarcoDayz <diaz.marco8686@gmail.com>
2022-12-10 11:18:53 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Instructions from "./components/Instructions";
+
+const App = () => {
+
+  return(
+    <>
+    <Instructions />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Instructions.js
@@ -0,0 +1,15 @@
+const Instructions = () => {
+
+    return (
+        <div>
+            <h1>Instructions:</h1>
+            <h3>- Create an application that makes an HTTP request to "https://jsonplaceholder.typicode.com/posts".</h3>
+            <h3>- Returned data must render all the post titles in a "Titles" component.</h3>
+            <h3>- Application should feature the capability to render a single post once it has been clicked on</h3>
+            <h3>- <span style={{color: 'blue'}}>*Bonues*</span> Create a return button that takes you back to all the posts <span style={{color: 'blue'}}>*Bonues*</span></h3>
+            <h1 style={{fontStyle: 'italic'}}>Create your application below</h1>
+        </div>
+    )
+};
+
+export default Instructions;
